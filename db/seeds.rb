fc6ce77677b163b53250b21e19b7244f8f327d05
thecodeplanner <thecodeplanner@gmail.com>
2021-02-08 14:58:12 -0500

    updated profile seeds image

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ Task.destroy_all
 Selfcare.destroy_all
 
 
-melissa = User.create(first_name: 'Melissa', last_name: 'Mach', image:"db/images/profile-icon.png", username: 'mmach', password: '123' )
-testUser = User.create(first_name: 'Edit', last_name: 'Name Here', image:"https://www.vhv.rs/dpng/d/548-5489482_download-icons-checklist-computer-black-text-white-checklist.png", username: 'testuser', password: '123')
+melissa = User.create(first_name: 'Melissa', last_name: 'Mach', image:"profile-icon.png", username: 'mmach', password: '123' )
+testUser = User.create(first_name: 'Edit', last_name: 'Name Here', image:"test-user-icon.png", username: 'testuser', password: '123')
 
 feb1 = Day.create(date: Date.new(2021,2,1), entry:"I'm grateful that I'm almost done with my coding bootcamp", water_intake: 0, user_id: melissa.id)
 feb2 = Day.create(date: Date.new(2021,2,2), entry: "all the small things in life", mood: "hopeful", water_intake: 0, user_id: melissa.id)
@@ -23,13 +23,13 @@ feb4 = Day.create(date: Date.new(2021,2,4), water_intake: 0, user_id: melissa.id
 feb5 = Day.create(date: Date.new(2021,2,5), water_intake: 0, user_id: melissa.id)
 
 
-laundry = Task.create(description: 'laundry', complete: false, day_id: feb2.id)
+laundry = Task.create(description: 'laundry', complete: true, day_id: feb2.id)
 
-study = Task.create(description: 'study for code challenge', complete: false, day_id: feb2.id)
+study = Task.create(description: 'study for code challenge', complete: true, day_id: feb2.id)
 
-doc = Task.create(description: 'schedule doc appt', complete: false, day_id: feb1.id)
+doc = Task.create(description: 'schedule doc appt', complete: true, day_id: feb1.id)
 
-workout = Selfcare.create(description: 'workout', complete: false, day_id: feb2.id)
+workout = Selfcare.create(description: 'workout', complete: true, day_id: feb2.id)
 
 meditate = Selfcare.create(description: 'meditate for 10 minutes', complete: false, day_id: feb1.id)
 
